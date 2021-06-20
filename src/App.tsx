d4f2377596b9a3ec70a2c95dd99a5f250af4cb61
Ayman <ayman.patel97@gmail.com>
2021-06-20 16:16:05 +0530

    feat: NOBS#20 - Setup and properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback} from 'react';
 import './App.css';
 
+const Heading = ({title} : {title: string}) => <h2>{title}</h2>
+
+
+/** MOE: 
+ * Type '({ children }: { children?: ReactNode; }) => void' is not assignable to type 'FunctionComponent<{}>'.
+   Type 'void' is not assignable to type 'ReactElement<any, any> | null
+ const Box: React.FunctionComponent = ({children}) => {
+  <div
+  style={{
+    padding: "1rem",
+  }}>
+    {children}
+  </div>
+}
+ */
+
+const Box: React.FunctionComponent = ({children}) => (
+  <div
+  style={{
+    padding: "1rem",
+    fontWeight: "bold",
+    color: "#4287f5"
+
+  }}>
+    {children}
+  </div>
+)
+
+
+// onClick? : Optional
+// onClick?. : Optional chain
+const List: React.FunctionComponent<{
+  items: string[];
+  onClick?: (item:string) => void; // onClick is Optional
+}> = ({items, onClick}) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index} onClick={() => onClick?.(item)}>  
+        {item}
+      </li>
+    ))}
+  </ul>
+)
+
+
 function App() {
+
+  const onListClick = useCallback(
+    (item: string) => {
+      alert(item)
+    },
+    [], // dependency array
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading title="Introduction"/>
+      <Box>Hello There</Box>
+      <List items={["ayman", "sam", "altman"]} onClick={onListClick}/>
     </div>
   );
 }
