5acd081a741a0f9af64d884b8bd811f98f8524b6
Ayman <ayman.patel97@gmail.com>
2021-07-17 09:58:19 +0530

    remove unccessary things

--- src/interfaces/route.ts
+++
@@ -1,8 +0,0 @@
-export default interface IRoute {
-    
-    path: string,   // 
-    name: string,   // 
-    exact: boolean, // 
-    component: any, // Find better solution than `any` 
-    props?: any     // 
-}
